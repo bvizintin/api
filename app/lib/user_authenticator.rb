4bcd2223817a8e3c0057b771c4de694c0e20cb8d
bvizintin <boris.vizintin@gmail.com>
2021-07-19 15:10:58 +0200

    minor tweaks

--- app/lib/user_authenticator.rb
+++ app/lib/user_authenticator.rb
@@ -11,11 +11,18 @@ class UserAuthenticator
       client_id: ENV['GITHUB_CLIENT_ID'],
       client_secret: ENV['GITHUB_CLIENT_SECRET']
     )
-    res = client.exchange_code_for_token(code)   # ova exchange metoda šalje naš kod githubu, koji vraća access token
-    if res.error.present?
+    token = client.exchange_code_for_token(code)   # ova exchange metoda šalje naš kod githubu, koji vraća access token
+    if token.try(:error).present?
       raise AuthenticationError
     else
+      user_client = Octokit::Client.new( access_token: token)
+      user_data = user_client.user.to_h.slice(:login, :avatar_url, :url, :name)            # .user metoda kada se poziva na Octokit objektu vraća sve moguće podatke o Useru u obliku Sayer objekta. .to_h ga pretvara u korisni hash iz kojeg slajsamo samo kaj nam treba
 
+      if User.exists?(login: user_data[:login])
+        @user = User.find_by(login: user_data[:login])
+      else
+        @user = User.create(user_data.merge(provider: "github"))
+      end
     end
   end
 
--- app/lib/user_authenticator_spec.rb
+++ app/lib/user_authenticator_spec.rb
@@ -5,10 +5,12 @@ describe UserAuthenticator do
     let(:authenticator) {described_class.new("neki-kod")}      #ovo se poziva prije svakog example-a u kojemu se koristi.
 
     context "when code is invalid" do
+      let(:error) { double("Sawyer::Resource", error: "bad_verification_code")}  #Sawyer je samo posebna vrsta Hash objekta koju Octokit vraća kada ima invalid verification kod
+
       before do
-        allow_any_instance_of(Octokit::CLient).to receive(:exchange_code_for_token).and_return(error)   #kad god se poziva :exchange metoda na bilo kojem Octokit::Client instanci, nemoj pozivati stvarnu :exchange metodu nego samo vrati error object. Ovaj override koristimo jer ne želimo za TESTIRANJE za stvarno slati zahtjeve githubu, niti ovisiti o internet vezi
+        allow_any_instance_of(Octokit::Client).to receive(:exchange_code_for_token).and_return(error)   #kad god se poziva :exchange metoda na bilo kojem Octokit::Client instanci, nemoj pozivati stvarnu :exchange metodu nego samo vrati error object. Ovaj override koristimo jer ne želimo za TESTIRANJE za stvarno slati zahtjeve githubu, niti ovisiti o internet vezi
       end
-      
+
       it "should raise an error" do
         expect {authenticator.perform}.to raise_error(UserAuthenticator::AuthenticationError)   #pazi na {}. Postoji expect() i expect{}
         expect(authenticator.user).to be_nil
@@ -16,9 +18,29 @@ describe UserAuthenticator do
     end
 
     context "when code is valid" do
-      it "should save the new user" do
-        expect(authenticator.perform).to change{ User.count }.by(1)
+      let(:user_data) do
+        {
+          login: "jwick",
+          url: "http://example.com",
+          avatar_url: "http://example.com/avatar",
+          name: "John Wick"
+        }
       end
+
+      before do
+        allow_any_instance_of(Octokit::Client).to receive(:exchange_code_for_token).and_return("validaccesstoken")   #kad god se poziva :exchange metoda na bilo kojem Octokit::Client instanci, nemoj pozivati stvarnu :exchange metodu nego samo vrati error object. Ovaj override koristimo jer ne želimo za TESTIRANJE za stvarno slati zahtjeve githubu, niti ovisiti o internet vezi
+        allow_any_instance_of(Octokit::Client).to receive(:user).and_return(user_data)
+      end
+      it "should save the new user" do
+        expect{authenticator.perform}.to change{ User.count }.by(1)
+      end
+
+      it "shoul reuse already registered user" do
+        user = create :user, user_data
+        expect{authenticator.perform}.not_to change{User.count}
+        expect(authenticator.user).to eq(user)
+      end
+
     end
   end
 end
